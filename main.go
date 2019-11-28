e360e6f6fae8c2854e35c9d954959b9bf7033167
dhr <616011986@qq.com>
2019-11-28 12:39:55 +0800

    修改项目结构

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"admin/models"
+	_ "admin/routers"
+	"fmt"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"mime"
+)
+
+const VERSION = "0.1.2"
+
+func initialize() {
+	mime.AddExtensionType(".css", "text/css")
+	//判断初始化参数
+	initArgs()
+
+	models.Connect()
+
+}
+func initArgs() {
+	models.Syncdb()
+	//args := os.Args
+	//for _, v := range args {
+	//	if v == "-syncdb" {
+	//		models.Syncdb()
+	//		os.Exit(0)
+	//	}
+	//}
+}
+
+func main() {
+	//初始化
+	initialize()
+
+	fmt.Println("Starting....")
+
+	fmt.Println("Start ok")
+	// 开启 ORM 调试模式
+	orm.Debug = true
+	// 自动建表
+	orm.RunSyncdb("default", false, true)
+	beego.AddFuncMap("stringsToJson", models.StringsToJson)
+	//
+	// 运行时
+	beego.Run()
+}
